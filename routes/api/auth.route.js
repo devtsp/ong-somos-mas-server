ba94ca828a0a6452f08410e5c2b59cae48fe3bf2
AlandelFresno <alandelfresno2@gmail.com>
2022-07-15 19:49:35 -0300

    Store user in DB
    Store new user in database.

--- routes/api/auth.route.js
+++ routes/api/auth.route.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../../models/user');
+const db = require('../../models/index');
 
 router.post('/register', (req, res) => {
-    
+
+    const {body} = req;
+    const user = db.User.build({
+        firstName: body.firstName,
+        lastName: body.lastName,
+        email: body.email,
+        password: body.password,
+        image: body.image,
+        roleId: 1,
+    });
+    user.save();
+    res.json(user);
 });
 
 
