a916129561350982fd0962e2d09a75914822b086
marianosim <mariano.simantob@gmail.com>
2022-07-16 13:40:25 -0300

    addActivity service created

---
+++ services/activities.service.js
@@ -0,0 +1,8 @@
+//const { Activities } = require('./models/activities.js'); 
+//(Model activities todavía no creado)
+
+const addActivity = ({name, content}) => {
+    //Activity.create({name, content});
+};
+
+module.exports = { addActivity };
