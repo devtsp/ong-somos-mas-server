143112dff43a817daf6090afd7a65ed3356305dc
Tomas Paez <paeztms@gmail.com>
2022-07-17 03:04:03 -0300

    remove console.log

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,11 +1,10 @@
 const verifyRoles = (...allowedRoles) => {
-	return (req, res, next) => {
-		console.log(req);
-		if (!req?.user?.role) return res.sendStatus(401);
-		const isAuthorized = allowedRoles.includes(req.user.role);
-		if (!isAuthorized) return res.sendStatus(401);
-		next();
-	};
+  return (req, res, next) => {
+    if (!req?.user?.role) return res.sendStatus(401);
+    const isAuthorized = allowedRoles.includes(req.user.role);
+    if (!isAuthorized) return res.sendStatus(401);
+    next();
+  };
 };
 
 module.exports = verifyRoles;
