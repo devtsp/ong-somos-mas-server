4359590ec0e9aed73435875657843e7dcea1747f
marianosim <mariano.simantob@gmail.com>
2022-07-18 15:06:18 -0300

    Minor bug fix (typo)

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const verifyJWT = require('./middleware/verifyJWT');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const membersRouter = require('./routes/members.route.js');
-const activitiesRouter = require('.routes/activities.route.js');
+const activitiesRouter = require('./routes/activities.route.js');
 const apiRouter = require('./routes/api/index');
 
 const app = express();
