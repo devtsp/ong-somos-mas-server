caf0203d0cdd23f008511946644f75048d3c8495
emiliof <emiliofurrer00@gmail.com>
2022-07-19 19:24:14 -0300

    added basic PUT activity endpoint to activities.route.js

--- routes/activities.route.js
+++ routes/activities.route.js
@@ -6,20 +6,23 @@ const ROLES_LIST = require('../config/rolesList');
 
 const { postActivities } = require('../controllers/activities.controller.js');
 
+const fieldValidations = [
+  check('name', 'Name is required'.not().isEmpty()),
+  check('content', 'Content is required'.not().isEmpty()),
+];
+
 //@type POST
 //@route /api/activities
-//@desc creates new activity. Checks 'name' and 'content' fields to not be empty. 
+//@desc creates new activity. Checks 'name' and 'content' fields to not be empty.
 //@access Private
 
+router.post('/', verifyRoles(ROLES_LIST.Admin), fieldValidations, postActivities);
 
-router.post(
-    '/',
-    verifyRoles(ROLES_LIST.Admin),
-    [
-        check('name', 'Name is required'.not().isEmpty()),
-        check('content', 'Content is required'.not().isEmpty()),
-    ],
-    postActivities
-);
+//@type PUT
+//@route /api/activities
+//@desc updates activity for a given id. Checks 'name' and 'content' fields to not be empty.
+//@access Private
+
+router.put('/:id', verifyRoles(ROLES_LIST.Admin), fieldValidations, putActivities);
 
 module.exports = router;
