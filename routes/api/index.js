8b96bb178a68d0c74341c6dea76ddaca8e35bc45
Andres Nazzari <lordcliff@gmail.com>
2022-07-19 21:01:29 +0900

    ✨ NEW FEATURE: endpoint POST Categories

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/news', require('./news.route'));
-
+router.use('categories', require('./categories.route'));
 module.exports = router;
