8aeda29f3aeb23cb23fb7bc930412ad5396f9fde
Andres Nazzari <lordcliff@gmail.com>
2022-07-19 10:13:27 +0900

    edit news functions and exports

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,12 +1,18 @@
-const { postNewService } = require('../services/news.service');
+const { postNewService, editNews, newsExists } = require('../services/news.service');
 
 const postNew = async (req, res) => {
-	if (!req?.body) {
-		return res.status(404).json({ errors: 'request body missing' });
-	}
-	req.body.type = 'news';
-	const result = await postNewService(req.body);
-	res.json(result);
+  if (!req?.body) {
+    return res.status(404).json({ errors: 'request body missing' });
+  }
+  req.body.type = 'news';
+  const result = await postNewService(req.body);
+  res.json(result);
+};
+
+const putNews = async (req, res) => {
+  if (!req?.body) {
+    return res.status(404).json({ errors: 'request body missing' });
+  }
 };
 
 module.exports = { postNew };
--- services/news.service.js
+++ services/news.service.js
@@ -1,5 +1,15 @@
-const postNewService = async postBody => {
-	return postBody;
+const postNewService = async (postBody) => {
+  return postBody;
 };
 
-module.exports = { postNewService };
+const editNews = async ({ id, name, image, updatedAt }) => {
+  //const news = await db.Entry.update({ name, image, updatedAt }, { where: { id } });
+  return news;
+};
+
+const newsExists = async ({ id }) => {
+  const newsExists = await db.Entry.findByPk(id);
+  return newsExists ? newsExists : null;
+};
+
+module.exports = { postNewService, editNews, newsExists };
