71248ac9cdd098335beadac8dec57b8a729d6f4c
emiliof <emiliofurrer00@gmail.com>
2022-07-19 19:08:57 -0300

    refactored testimonials.route code and added validations to PUT endpoint

--- routes/testimonials.route.js
+++ routes/testimonials.route.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-
 const { postTestimonial, putTestimonial } = require('../controllers/testimonials.controller.js');
+
+const fieldValidations = [
+  check('name', 'Name is required').not().isEmpty(),
+  check('content', 'Content is required').not().isEmpty(),
+];
 
 //@type POST
 //@route /api/testimonials
 //@desc creates a new testimonial. Checks 'name' and 'content' fields not to be empty;
 //@access Private
-router.post(
-  '/',
-  [
-    check('name', 'Name is required').not().isEmpty(),
-    check('content', 'Content is required').not().isEmpty(),
-  ],
-  postTestimonial
-);
+router.post('/', fieldValidations, postTestimonial);
 
 //@type PUT
 //@route /api/testimonials
 //@desc updates a testimonial for a given id param.
 //@access Private
-router.put('/:id', putTestimonial);
+router.put('/:id', fieldValidations, putTestimonial);
 
 module.exports = router;
