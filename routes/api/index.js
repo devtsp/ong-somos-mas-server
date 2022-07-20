4989ef5808f15e0c45bb13e0a71ac83436f41a48
AlandelFresno <alandelfresno2@gmail.com>
2022-07-19 21:01:18 -0300

    Fix: 404 error
    Fixed error 404,
    Added an use to the correct path and file in app.js,
    in api\index.js path corrected

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/api/auth', require('./auth.route'));
+router.use('/auth', require('./auth.route'));
 
 module.exports = router;
