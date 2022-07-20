8587bf4e21455b4270726a54e1f6805861d611ce
emiliof <emiliofurrer00@gmail.com>
2022-07-20 16:19:45 -0300

    added neccessary controller import to activities router

--- routes/activities.route.js
+++ routes/activities.route.js
@@ -4,7 +4,7 @@ const { check } = require('express-validator');
 const verifyRoles = require('../middleware/verifyRoles');
 const ROLES_LIST = require('../config/rolesList');
 
-const { postActivities } = require('../controllers/activities.controller.js');
+const { postActivities, putActivity } = require('../controllers/activities.controller.js');
 
 const fieldValidations = [
   check('name', 'Name is required'.not().isEmpty()),
@@ -23,6 +23,6 @@ router.post('/', verifyRoles(ROLES_LIST.Admin), fieldValidations, postActivities
 //@desc updates activity for a given id. Checks 'name' and 'content' fields to not be empty.
 //@access Private
 
-router.put('/:id', verifyRoles(ROLES_LIST.Admin), fieldValidations, putActivities);
+router.put('/:id', verifyRoles(ROLES_LIST.Admin), fieldValidations, putActivity);
 
 module.exports = router;
