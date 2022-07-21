b33c78c2e777c498e0a9bd50677d7464643f37da
AlandelFresno <alandelfresno2@gmail.com>
2022-07-21 03:17:48 -0300

    Revert "Fix: conflicts"
    This reverts commit db940125b4ecbff3d8fcc46bc16b11d915272b6b.

--- README.md
+++ README.md
@@ -26,23 +26,3 @@ npx sequelize-cli db:seed:all
 ``` bash
 npm start
 ```
-
-## Seed de usuarios
-
-Al ejecutarse el seed de usuarios se crean en la tabla users 10 usuarios administradores (roleId = 1) y 10 usuarios regulares (roleId = 2).
-Ejemplo de usuario administrador:
-
-``` 
-    {
-        firstName: 'Usuario5',
-        lastName: 'Demo',
-        email: 'test5@test.com',
-        // Important: Password not encrypted yet! 
-        password: '1234',
-        roleId: 1,
-        image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-        createdAt: new Date,
-        updatedAt: new Date
-    }
-
-```
--- controllers/activities.controller.js
+++ controllers/activities.controller.js
@@ -1,43 +1,20 @@
 const { validationResult } = require('express-validator');
-const { addActivity, findActivity, updateActivity } = require('../services/activities.service.js');
+const addActivity = require('../services/activities.service.js');
 
 const postActivities = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  const { name, content } = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    const { name, content } = req.body;
 
-  try {
-    await addActivity(name, content);
-    res.status(200).json({ msg: 'Activity created succesfully' });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send(`Server error: ${error.message}`);
-  }
+    try{
+        await addActivity(name, content);
+        res.status(200).json({ msg: 'Activity created succesfully'});
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).send(`Server error: ${error.message}`);
+    }
 };
 
-const putActivity = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { id } = req.params;
-
-  const activityToUpdate = findActivity(id);
-  if (activityToUpdate === null) {
-    return res.status(404).json({ error: 'There is no activity with the given id' });
-  }
-
-  const { name, content } = req.body;
-
-  try {
-    const updatedActivity = await updateActivity({ name, content });
-    res.status(200).json({ msg: `Activity succesfully updated`, activity: { name, content } });
-  } catch (error) {
-    res.status(500).json({ errors: error.message });
-  }
-};
-
-module.exports = { postActivities, putActivity };
+module.exports = { postActivities };
