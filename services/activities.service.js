15f4e1a3038364bd4dfbe8e8387441e80828fb3e
emiliof <emiliofurrer00@gmail.com>
2022-07-21 19:27:16 -0300

    same change as previous commit but for activities.service import

--- services/activities.service.js
+++ services/activities.service.js
@@ -1,16 +1,16 @@
-const { activities } = require('../models');
+const { Activity } = require('../models');
 
 const addActivity = ({ name, content }) => {
   //Activity.create({name, content});
 };
 
 const findActivity = async (id) => {
-  const activity = await activities.findOne({ where: { id: id } });
+  const activity = await Activity.findOne({ where: { id: id } });
   return activity;
 };
 
 const updateActivity = async (activityInstance, newValues) => {
-  const updatedActivity = await activities.update(newValues);
+  const updatedActivity = await Activity.update(newValues);
   return updatedActivity;
 };
 
