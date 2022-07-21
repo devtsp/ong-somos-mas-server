574dad598be14afbd9c4729f84296f38b031c613
emiliof <emiliofurrer00@gmail.com>
2022-07-21 19:37:17 -0300

    deleted unneccesary console.log

--- services/testimonials.service.js
+++ services/testimonials.service.js
@@ -2,7 +2,6 @@ const { Testimonial } = require('../models');
 
 const addTestimonial = async (testimonialData) => {
   const newTestimonial = await Testimonial.create({ ...testimonialData });
-  console.log(newTestimonial);
   //Mock response:
   return newTestimonial;
 };
