e3a5e1598539aa885faad7cc13cefc6ebb5f0572
Mauricio <mauri.yapura@yahoo.com>
2022-07-21 22:36:10 -0300

    mini fix

--- services/categories.service.js
+++ services/categories.service.js
@@ -1,9 +1,9 @@
 const db = require('../models/index.js');
 
-const addCategory = async ({ name, description, createdAt, updatedAt }) => {
-  const category = db.Category.build({ name, description, createdAt, updatedAt });
-  await category.save();
-  return category;
+const addCategory = async ({ name, description, createdAt,updatedAt }) => {
+    const category = db.Category.build({ name, description, createdAt,updatedAt });
+    await category.save();
+    return category;
 };
 
 const categoryExists = async ({ id }) => {
