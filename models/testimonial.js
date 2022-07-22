81fb39a37c6c665ca15ddbef26b307a688271e95
emiliof <emiliofurrer00@gmail.com>
2022-07-22 18:58:02 -0300

    added image column back to Testimonial model

--- models/testimonial.js
+++ models/testimonial.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: DataTypes.STRING,
       content: DataTypes.STRING,
+      image: DataTypes.STRING,
     },
     {
       sequelize,
