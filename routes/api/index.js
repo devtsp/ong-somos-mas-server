d682a8efe300c632b998ed32894957cee615c795
Andres Nazzari <lordcliff@gmail.com>
2022-07-24 12:23:42 +0900

    Add missing postCategory route controller OT240-97

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.use('/auth', require('./auth.route'));
 router.use('/news', require('./news.route'));
 router.use('/members', require('./members.route'));
-
+router.use('categories', require('./categories.route'));
 
 module.exports = router;
