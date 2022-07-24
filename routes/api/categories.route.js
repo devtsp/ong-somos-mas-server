aa7ee11daa021e4f6b0f69f6db1265846b86baa4
Andres Nazzari <lordcliff@gmail.com>
2022-07-24 16:08:43 +0900

    Add PUT category route

--- routes/api/categories.route.js
+++ routes/api/categories.route.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 
-const { postCategory, deleteCategory } = require('../../controllers/categories.controller');
+const {
+  postCategory,
+  deleteCategory,
+  putCategory,
+} = require('../../controllers/categories.controller');
 
 //@route    POTS /api/categories
 //@desc     Agregar una categoría nueva, valida que se envíe el campo name y que sea un string
@@ -14,6 +18,18 @@ router.post(
     check('name', 'Name must be string').isString(),
   ],
   postCategory
+);
+
+//@route    PUT /api/categories
+//@desc     Valida que la categoría existe y la actualiza, caso contrario devuelve un error
+//@access   Private
+router.put(
+  '/:id',
+  [
+    check('name', 'Name is Required').not().isEmpty(),
+    check('name', 'Name must be string').isString(),
+  ],
+  putCategory
 );
 
 //@route    POTS /api/categories
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 router.use('/auth', require('./auth.route'));
 router.use('/news', require('./news.route'));
 router.use('/members', require('./members.route'));
-router.use('categories', require('./categories.route'));
+router.use('/categories', require('./categories.route'));
 
 module.exports = router;
