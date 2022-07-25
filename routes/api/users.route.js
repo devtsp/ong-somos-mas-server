0a7fc4ddc4b9e439f17cf05fd5e9cc3c7afdaa29
Andres Nazzari <lordcliff@gmail.com>
2022-07-25 13:14:20 +0900

    added token validation to route

--- routes/api/users.route.js
+++ routes/api/users.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getUsers } = require('../../controllers/users.controller');
+const validateToken = require('../../middleware/validateToken');
 
 //@route    GET /api/users
 //@desc     Devuelve todos los Usuarios
 //@access   Private (Admin)
-router.get('/', getUsers);
+router.get('/', validateToken, getUsers);
 
 module.exports = router;
