5c75dc82eaed164189e05c243a927748646e2b74
emiliof <emiliofurrer00@gmail.com>
2022-07-25 17:03:55 -0300

    added basic structure for organizations.controller.js

---
+++ controllers/organizations.controller.js
@@ -0,0 +1,9 @@
+//const { /* Required services for organizations controller */ } = require('../services/organizations.service');
+
+const getOrganization = async (req, res) => {
+  // Here we should find if an organization with the giving id exists.
+  // If it does, we return the @public fields
+  // Otherwise, we should return some error explaining that instance doesn't exist
+};
+
+module.exports = { getOrganization };
