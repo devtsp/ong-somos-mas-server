8908e6100872b138af74579e094792f0661edfc0
Andres Nazzari <lordcliff@gmail.com>
2022-07-25 12:58:26 +0900

    Add user route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.use('/auth', require('./auth.route'));
 router.use('/news', require('./news.route'));
 router.use('/members', require('./members.route'));
-
+router.use('/users', require('./users.route'));
 
 module.exports = router;
