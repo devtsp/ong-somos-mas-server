1fe1f71a2997203f465e79d28d1e5ae65b331367
Andres Nazzari <lordcliff@gmail.com>
2022-07-25 13:35:40 +0900

    Added Role and Token Verification

--- routes/api/users.route.js
+++ routes/api/users.route.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const { getUsers } = require('../../controllers/users.controller');
 const validateToken = require('../../middleware/validateToken');
 
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/rolesList');
+
 //@route    GET /api/users
 //@desc     Devuelve todos los Usuarios
 //@access   Private (Admin)
-router.get('/', validateToken, getUsers);
+router.get('/', [validateToken, verifyRoles(ROLES_LIST.Admin)], getUsers);
 
 module.exports = router;
