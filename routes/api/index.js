c54cb5767ab25cb01051024af2103775d86dc8a7
Mauricio <mauri.yapura@yahoo.com>
2022-07-25 23:49:14 -0300

    endpoint get all contacts

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.use('/auth', require('./auth.route'));
 router.use('/news', require('./news.route'));
 router.use('/members', require('./members.route'));
+router.use('/contacts', require('./contacts.route'));
 
 
 module.exports = router;
