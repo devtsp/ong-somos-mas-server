d601c156584f4c0007ed785be7a0dc8bb9372508
Alan del Fresno <alandelfresno2@gmail.com>
2022-07-26 20:08:01 -0300

    Fix: Soft delete
    Changed Hard delete for Soft delete

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,15 +5,15 @@ const userDelete = async(req, res) => {
     const user = await db.User.findOne({where: {id : req.params.id}});
     
     if ( user !== null) {
-        db.User.destroy({
-            where: {
-                id: req.params.id
+        await db.User.update(
+            {deletedAt: new Date}, 
+            {
+                where: {
+                    id: req.params.id
+                }
             }
-        }).then(() => {
-            res.json(user).status(200);
-        }).catch((err) => {
-            res.json(err).status(500);
-        });
+        );
+        res.status(200).json(user);
     } else {
         res.status(404).json({msg: "User not found"});
     };
