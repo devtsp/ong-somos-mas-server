f049e204973ac9c482a66b548986262141dcdd40
Alan del Fresno <alandelfresno2@gmail.com>
2022-07-26 19:45:03 -0300

    Validate Token
    Check if user is logged ( has token)

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
-
 const { userDelete } = require('../controllers/users.controller');
+const validateToken = require('../middleware/validateToken');
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -9,7 +10,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.delete('/:id', userDelete);
+//@route    DELETe /users/:id
+//@desc     Delete my user
+//@access   Private
+router.delete('/:id', validateToken, userDelete);
 
 
 module.exports = router;
