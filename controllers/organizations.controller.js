e1980c4b4a13a8f9126e751761244e8e75c5f14e
emiliof <emiliofurrer00@gmail.com>
2022-07-27 18:01:34 -0300

    added organizationId req param use on organization.controller

--- controllers/organizations.controller.js
+++ controllers/organizations.controller.js
@@ -4,9 +4,10 @@ const getOrganization = async (req, res) => {
   // Here we should find if an organization with the giving id exists.
   // If it does, we return the @public fields
   // Otherwise, we should return some error explaining that instance doesn't exist
+  const { organizationId } = req.params;
   try {
     /* This should be uncommented when the service is implemented
-    
+
     const organization = findOrganization(id);
     if (organization == null){
       res.status(404).json({error: "There is no organization with the given id"})
@@ -22,6 +23,7 @@ const getOrganization = async (req, res) => {
       phone: 'Mock phone',
       address: 'Mock address',
       welcomeText: 'Mock welcomeText',
+      id: organizationId,
     });
   } catch (error) {
     res.status(500).json({ errors: error.message });
