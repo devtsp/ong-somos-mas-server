61add4687168ccdfd1735b7fd4d3cc407b6295e3
emiliof <emiliofurrer00@gmail.com>
2022-07-28 03:49:34 -0300

    increased welcomeText column char limit

--- migrations/20220728062858-create-organization.js
+++ migrations/20220728062858-create-organization.js
@@ -6,34 +6,34 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       phone: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       address: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       welcomeText: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(500),
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Organizations');
-  }
+  },
 };
