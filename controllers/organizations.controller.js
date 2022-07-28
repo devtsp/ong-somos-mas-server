a31f7a0470c0b96e3aabfbc36d2750c8b4ed461a
emiliof <emiliofurrer00@gmail.com>
2022-07-28 18:35:59 -0300

    added updateOrganizationById to the controller

--- controllers/organizations.controller.js
+++ controllers/organizations.controller.js
@@ -1,9 +1,9 @@
 //const { /* Required services for organizations controller */ } = require('../services/organizations.service');
 
-const getOrganization = async (req, res) => {
+const getOrganizationById = async (req, res) => {
   const { organizationId } = req.params;
   try {
-    const organization = findOrganization(organizationId);
+    const organization = await findOrganization(organizationId);
     if (organization == null) {
       res.status(404).json({ error: 'There is no organization with the given id' });
     }
@@ -19,6 +19,25 @@ const getOrganization = async (req, res) => {
       linkedin,
       instagram,
     });
+  } catch (error) {
+    res.status(500).json({ errors: error.message });
+  }
+};
+
+const updateOrganizationById = async (req, res) => {
+  const { organizationId } = req.params;
+  const newValues = req.body;
+  try {
+    const organization = await findOrganization(organizationId);
+    if (organization == null) {
+      res.status(404).json({ error: 'There is no organization with the given id' });
+    }
+    const updatedOrganization = await updateOrganization(organization, newValues);
+    const { name, image, phone, address, welcomeText, facebook, linkedin, instagram } =
+      updatedOrganization;
+    return res
+      .status(200)
+      .json({ name, image, phone, address, welcomeText, facebook, linkedin, instagram });
   } catch (error) {
     res.status(500).json({ errors: error.message });
   }
