41480d265a831b3ab0e461b51e4f6bc217e22550
Alan del Fresno <alandelfresno2@gmail.com>
2022-07-28 21:05:00 -0300

    User delete route
    Added user delete route

--- routes/api/users.route.js
+++ routes/api/users.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers } = require('../../controllers/users.controller');
+const { getUsers, userDelete } = require('../../controllers/users.controller');
 const validateToken = require('../../middleware/validateToken');
 
 const verifyRoles = require('../../middleware/verifyRoles');
@@ -10,5 +10,10 @@ const ROLES_LIST = require('../../config/rolesList');
 //@desc     Devuelve todos los Usuarios
 //@access   Private (Admin )
 router.get('/', [validateToken, verifyRoles(ROLES_LIST.Admin)], getUsers);
+
+//@route    DELETe /users/:id
+//@desc     Delete my user
+//@access   Private
+router.delete('/:id', validateToken, userDelete);
 
 module.exports = router;
