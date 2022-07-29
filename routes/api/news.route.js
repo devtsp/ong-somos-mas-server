95acca56f960ccadd7c2d5cd03613736e6b73163
Andres Nazzari <lordcliff@gmail.com>
2022-07-29 16:48:45 +0900

    added news routes proteccion

--- routes/api/news.route.js
+++ routes/api/news.route.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
+const validateToken = require('../../middleware/validateToken');
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/rolesList');
 
 const newsController = require('../../controllers/news.controller');
 
 //@route    GET /api/news/:id
 //@desc     Retrieve entry of type: "news" by id
-//@access   Private (Admin)
+//@access   Public
 router.get('/:id', newsController.getNewById);
 
 //@route    GET /api/news
 //@desc     Retrieve all entries of type: "news"
-//@access   Private (Admin)
+//@access   Public
 router.get('/', newsController.getNews);
 
 //@route    POST /api/news
 //@desc     Post entry of type: "news"
 //@access   Private (Admin)
-router.post('/', newsController.postNew);
+router.post('/', [validateToken, verifyRoles(ROLES_LIST.Admin)], newsController.postNew);
 
 //@route    PUT /api/news
 //@desc     Actualizar una novedad existente
 //@access   Private
 router.put(
   '/:id',
+  [validateToken, verifyRoles(ROLES_LIST.Admin)],
   [check('name', 'name must be string').isString()],
   [check('content', 'content must be string').isString()],
   [check('image', 'image must be string').isString()],
@@ -35,6 +39,6 @@ router.put(
 //@route    DELETE /api/news/:id
 //@desc     Post entry of type: "news"
 //@access   Private (Admin)
-router.delete('/:id', newsController.deleteNew);
+router.delete('/:id', [validateToken, verifyRoles(ROLES_LIST.Admin)], newsController.deleteNew);
 
 module.exports = router;
