570bbd75785f402f2f8d12a1c1cba2338a9d74f5
emiliof <emiliofurrer00@gmail.com>
2022-07-29 02:23:04 -0300

    Now PUT /api/organization endpoint is properly protected

--- routes/api/organization.route.js
+++ routes/api/organization.route.js
@@ -4,6 +4,9 @@ const {
   getOrganization,
   updateOrganization,
 } = require('../../controllers/organization.controller');
+const validateToken = require('../../middleware/validateToken');
+const verifyRoles = require('../../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/rolesList');
 
 //@route    GET api/organization/public
 //@desc     Gets a organization public data for a given organizationId
@@ -13,6 +16,6 @@ router.get('/public', getOrganization);
 //@route    PUT api/organization
 //@desc     Updates a organization data for a given organizationId
 //@access   private
-router.put('/', updateOrganization);
+router.put('/', validateToken, verifyRoles(ROLES_LIST.Admin), updateOrganization);
 
 module.exports = router;
