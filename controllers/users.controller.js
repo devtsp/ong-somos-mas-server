2799a54f2246c74e398b36937e873d19033dc454
Alan del Fresno <alandelfresno2@gmail.com>
2022-07-28 21:07:53 -0300

    Improve userDelete
    Improvements to userDelete function;
    Checks if it's an admin,
    Checks if it is the same user as the one to be deleted.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,5 @@
+const { default: jwtDecode } = require('jwt-decode');
+
 const db = require('../models/index');
 const { getAllUsers } = require('../services/users.service');
 
@@ -11,21 +13,43 @@ const getUsers = async (req, res) => {
 };
 
 const userDelete = async (req, res) => {
+
   const user = await db.User.findOne({ where: { id: req.params.id } });
+  const token = req.token.split(' ')[1];
+  const userDataToken = jwtDecode(token);
 
   if (user !== null) {
-    await db.User.update(
-      { deletedAt: new Date() },
-      {
-        where: {
-          id: req.params.id,
-        },
-      }
-    );
-    res.status(200).json(user);
+    if( userDataToken.UserInfo.roleId == 1){
+
+      try {
+        await db.User.update(
+          { deletedAt: new Date() },
+          { where: {id: req.params.id,} }
+        );
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json(error);
+      };
+
+    }else if (userDataToken.UserInfo.id == req.params.id) {
+
+      try {
+        await db.User.update(
+          { deletedAt: new Date() },
+          { where: {id: req.params.id,} }
+        );
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(500).json(error);
+      };
+
+    }else {
+      res.status(400).json({msg: "You can't delete other users"});
+    };
+
   } else {
     res.status(404).json({ msg: 'User not found' });
-  }
+  };
 };
 
 module.exports = { userDelete, getUsers };
