e7111d69fd3a543d76628d721b2d1be3e2f34b27
Tomas Paez <paeztms@gmail.com>
2022-07-30 16:49:29 -0300

    add request validations to POST /news

---
+++ utils/validators.js
@@ -0,0 +1,36 @@
+const { body } = require('express-validator');
+
+const postEntryValidations = [
+  body('name')
+    .exists()
+    .withMessage('"name" field is required')
+    .bail()
+    .isString()
+    .withMessage('"name" field must be string')
+    .bail()
+    .trim()
+    .notEmpty()
+    .withMessage('"name" field cannot be empty'),
+  body('content')
+    .exists()
+    .withMessage('"content" field is required')
+    .bail()
+    .isString()
+    .withMessage('"content" field must be string')
+    .bail()
+    .trim()
+    .notEmpty()
+    .withMessage('"content" field cannot be empty'),
+  body('image')
+    .exists()
+    .withMessage('"image" field is required')
+    .bail()
+    .isString()
+    .withMessage('"image" field must be string')
+    .bail()
+    .trim()
+    .notEmpty()
+    .withMessage('"image" field cannot be empty'),
+];
+
+module.exports = { postEntryValidations };
