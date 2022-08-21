cd75dd837f83b0c50ad1e28c5306ce1be5e13c25
Tomas Paez <paeztms@gmail.com>
2022-08-21 04:17:40 -0300

    paranoid applied to user, auth fixes (return token with id hitting database after register success)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -12,12 +12,12 @@ const login = async (req, res) => {
 
   if (!err.isEmpty()) {
     return res.status(400).json({ err: err.array() });
-  };
+  }
 
   // Verify if email already exists
   const databaseUser = await db.User.findOne({ where: { email } });
 
-  if (databaseUser === null) {
+  if (!databaseUser) {
     return res.status(404).json({ msg: "User with that email doesn't exist" });
   }
 
@@ -26,18 +26,18 @@ const login = async (req, res) => {
 
   if (comparationResult === false) {
     return res.status(401).json({ msg: 'Invalid credentials' });
-  };
+  }
   // return to user if it exist and the password is valid
   const newToken = generateToken(databaseUser.dataValues);
-  if (databaseUser.deletedAt == null){
+  if (databaseUser.deletedAt == null) {
     return res.status(200).json({
       msg: 'Logged successfully',
       user: databaseUser,
       token: newToken,
     });
-  }else {
-    res.status(403).json({msg: "User is deleted, contact support for more information"})
-  };
+  } else {
+    res.status(400).json({ msg: "User doesn't exists" });
+  }
 };
 
 const register = async (req, res) => {
@@ -45,12 +45,17 @@ const register = async (req, res) => {
 
   if (!err.isEmpty()) {
     return res.status(400).json({ err: err.array() });
-  };
+  }
 
   const { body } = req;
   const userFound = await db.User.findOne({ where: { email: body.email } });
 
+  if (userFound) {
+    return res.status(400).json({ msg: 'User already exists with that email' });
+  }
+
   const password = await encryptPassword(body.password);
+
   const userData = {
     firstName: body.firstName,
     lastName: body.lastName,
@@ -59,27 +64,20 @@ const register = async (req, res) => {
     image: body.image,
     roleId: body.roleId || ROLES_LIST.User,
   };
-  const newToken = generateToken(userData);
-  
-  if (userFound == null || userFound.deletedAt !== null) {
-    const user = db.User.build(userData);
-    user
-    .save()
-    .then(() => {
-      res.status(200).json({ user, token: newToken });
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-  } else {
-    res.status(400).json({ msg: 'User already exists with that email' });
-  };
+
+  try {
+    const newUser = await db.User.create(userData);
+    const token = generateToken(newUser);
+    return res.json({ user: newUser, token });
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
 };
 
 const authMe = (req, res) => {
   const token = req.token.split(' ')[1];
-  const {UserInfo} = jwtDecode(token);
-  res.status(200).json({user: UserInfo});
+  const { UserInfo } = jwtDecode(token);
+  res.status(200).json({ user: UserInfo });
 };
 
 module.exports = { register, login, authMe };
